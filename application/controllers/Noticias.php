f55cf3d6b51fb20e8e628500fd83691b3ca2c50b
maikon cerezolli <maikonjcerezolli@gmail.com>
2014-12-01 19:44:55 -0200

    aula 44 - inciando a pagina de noticias

--- application/controllers/Noticias.php
+++ application/controllers/Noticias.php
@@ -10,8 +10,15 @@ class Noticias extends CI_Controller {
         $this->load->model('login_model', 'login');
         
     }
-    
-    public function index() {
+
+    public function index()
+	{
+		$this->load->view('home_header');
+                $this->load->view('home_content');
+                $this->load->view('home_sidebar');
+	}
+        
+    public function cadastro() {
         
         if($this->login->logado()){
         //$this->load->view('login/area_restrita_view');
